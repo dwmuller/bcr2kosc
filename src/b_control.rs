4e0da626c5da7a306c4b499d37dd986b8fb60e1d
Dan Muller <dan.muller@dmuller.us>
2022-10-16 12:10:40 -0400

    Rust code formatting is now working.

--- src/b_control.rs
+++ src/b_control.rs
@@ -2,25 +2,25 @@
 //!
 //! Types to represent system exclusive messages for Behringer's
 //! BCR2000 and BCF2000 MIDI controllers.
-//! 
-//! This is based on the amazing reverse engineering work by Mark van den 
+//!
+//! This is based on the amazing reverse engineering work by Mark van den
 //! Berg, published on https://mountainutilities.eu/. It follows patterns
 //! used in the midi_msg library crate and uses some types from it.
-//! 
+//!
 
 use midi_msg::ParseError;
-pub use midi_msg::{ManufacturerID, DeviceID};
+pub use midi_msg::{DeviceID, ManufacturerID};
 
 /// Behringer's MIDI manufacturer ID.
 pub const BEHRINGER: ManufacturerID = ManufacturerID(0x20u8, Some(0x32u8));
 
-/// B-Control mode system exclusive data. All system exclusive message data 
+/// B-Control mode system exclusive data. All system exclusive message data
 /// to or from the BC devices have this structure.
 #[derive(Debug, PartialEq, Eq)]
 pub struct BControlSysEx {
     pub device: DeviceID,
     pub model: BControlModel,
-    pub command: BControlCommand
+    pub command: BControlCommand,
 }
 
 impl BControlSysEx {
@@ -34,7 +34,7 @@ impl BControlSysEx {
             DeviceID::Device(d) => d.min(15),
             DeviceID::AllCall => 0x7f,
         });
-        v.push(match self.model{
+        v.push(match self.model {
             BControlModel::BCR => 0x15,
             BControlModel::BCF => 0x14,
             BControlModel::Any => 0x7f,
@@ -46,70 +46,94 @@ impl BControlSysEx {
             let device = match m[0] {
                 0..=15 => DeviceID::Device(m[0]),
                 0x7f => DeviceID::AllCall,
-                n => return Err(ParseError::Invalid(format!("Invalid device id. ({n})")))
+                n => return Err(ParseError::Invalid(format!("Invalid device id. ({n})"))),
             };
             let model = match m[1] {
                 0x14 => BControlModel::BCF,
                 0x15 => BControlModel::BCR,
                 0x7f => BControlModel::Any,
-                n => return Err(ParseError::Invalid(format!("Bad B-Control model number. ({n:x})")))
+                n => {
+                    return Err(ParseError::Invalid(format!(
+                        "Bad B-Control model number. ({n:x})"
+                    )))
+                }
             };
             let (command, used) = match m[2] {
                 0x01 => (BControlCommand::RequestIdentity, 0),
                 0x02 => (
                     BControlCommand::SendIdentity {
-                        id_string: string_from_midi(&m[3..])?
+                        id_string: string_from_midi(&m[3..])?,
                     },
-                    m.len()
+                    m.len(),
                 ),
-                0x20 => (BControlCommand::SendBclMessage { text: string_from_midi(&m[3..])? }, m.len()),
+                0x20 => (
+                    BControlCommand::SendBclMessage {
+                        text: string_from_midi(&m[3..])?,
+                    },
+                    m.len(),
+                ),
                 0x21 => {
                     if m.len() > 6 {
                         // Supposedly the preset name will be exactly 26 chars.
-                        (BControlCommand::SendPresetName {
-                            preset: PresetIndex::from_midi(&m[3..])?,
-                            name: string_from_midi(&m[4..])? 
-                        }, m.len())
+                        (
+                            BControlCommand::SendPresetName {
+                                preset: PresetIndex::from_midi(&m[3..])?,
+                                name: string_from_midi(&m[4..])?,
+                            },
+                            m.len(),
+                        )
                     } else {
                         (
                             BControlCommand::BclReply {
-                                msg_indx: u14_from_midi_msb_lsb(&m[3..])?, 
-                                error_code: u8_from_midi(&m[5..])? 
+                                msg_indx: u14_from_midi_msb_lsb(&m[3..])?,
+                                error_code: u8_from_midi(&m[5..])?,
                             },
-                            3
+                            3,
                         )
                     }
-                },
+                }
                 0x22 => (BControlCommand::SelectPreset { index: m[3] }, 1),
-                0x34 => (BControlCommand::SendFirmware { data: m[3..].to_vec() }, m.len()),
+                0x34 => (
+                    BControlCommand::SendFirmware {
+                        data: m[3..].to_vec(),
+                    },
+                    m.len(),
+                ),
                 0x35 => (
                     BControlCommand::FirmwareReply {
                         mem_addr: u14_from_midi_msb_lsb(&m[3..])?,
-                        err: u8_from_midi(&m[5..])?
+                        err: u8_from_midi(&m[5..])?,
                     },
-                    3
+                    3,
                 ),
                 0x40 => (
                     BControlCommand::RequestData {
-                        preset: PresetIndex::from_midi(&m[3..])?
+                        preset: PresetIndex::from_midi(&m[3..])?,
                     },
-                    1
+                    1,
                 ),
                 0x41 => (BControlCommand::RequestGlobalSetup, 0),
                 0x42 => (
                     BControlCommand::RequestPresetName {
-                        preset: PresetIndex::from_midi(&m[3..])?
+                        preset: PresetIndex::from_midi(&m[3..])?,
                     },
-                    1
+                    1,
                 ),
                 0x43 => (BControlCommand::RequestSnapshot, 0),
                 0x78 => (BControlCommand::SendText, 0),
-                cmd => return Err(ParseError::Invalid(format!("Invalid B-Control command {cmd:x}")))
+                cmd => {
+                    return Err(ParseError::Invalid(format!(
+                        "Invalid B-Control command {cmd:x}"
+                    )))
+                }
             };
-            let result = BControlSysEx {device, model, command};
+            let result = BControlSysEx {
+                device,
+                model,
+                command,
+            };
             Ok((result, used + 3))
-        }
-        else{
+        } else {
             Err(ParseError::UnexpectedEnd)
         }
     }
@@ -121,88 +145,118 @@ impl BControlSysEx {
 pub enum BControlModel {
     BCR,
     BCF,
-    Any
+    Any,
 }
-
 
 /// B-Control command data appears in system exclusive messages sent to or
 /// recieved from  B-Control devices.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum BControlCommand {
-    /// 
-    SendBclMessage { text: String },
+    ///
+    SendBclMessage {
+        text: String,
+    },
 
     RequestIdentity,
-    SelectPreset { index: u8 },
-    SendFirmware { data: Vec<u8> },
-    RequestData { preset: PresetIndex },
+    SelectPreset {
+        index: u8,
+    },
+    SendFirmware {
+        data: Vec<u8>,
+    },
+    RequestData {
+        preset: PresetIndex,
+    },
     RequestGlobalSetup,
-    RequestPresetName { preset: PresetIndex },
+    RequestPresetName {
+        preset: PresetIndex,
+    },
     RequestSnapshot,
 
-    SendIdentity { id_string: String },
-    BclReply { msg_indx: u16, error_code: u8 },
-    SendPresetName { preset: PresetIndex, name: String },
-    FirmwareReply { mem_addr: u16, err: u8 },
-    SendText
-
+    SendIdentity {
+        id_string: String,
+    },
+    BclReply {
+        msg_indx: u16,
+        error_code: u8,
+    },
+    SendPresetName {
+        preset: PresetIndex,
+        name: String,
+    },
+    FirmwareReply {
+        mem_addr: u16,
+        err: u8,
+    },
+    SendText,
 }
 impl BControlCommand {
     pub fn extend_midi(&self, v: &mut Vec<u8>) {
         match self {
-            BControlCommand::RequestIdentity => { v.push(0x01); },
+            BControlCommand::RequestIdentity => {
+                v.push(0x01);
+            }
             BControlCommand::SendBclMessage { text } => {
                 v.push(0x02);
-                extend_midi_from_string(text,v);
-            },
+                extend_midi_from_string(text, v);
+            }
             BControlCommand::SelectPreset { index } => {
                 v.push(0x22);
                 v.push(*index);
-            },
+            }
             BControlCommand::SendFirmware { data } => {
                 v.push(0x34);
                 data.iter().for_each(|b| v.push(*b));
-            },
+            }
             BControlCommand::RequestData { preset } => {
                 v.push(0x40);
                 preset.extend_midi(v);
-            },
-            BControlCommand::RequestGlobalSetup => { v.push(0x41); },
+            }
+            BControlCommand::RequestGlobalSetup => {
+                v.push(0x41);
+            }
             BControlCommand::RequestPresetName { preset } => {
-                v.push(0x42); preset.extend_midi(v);
-            },
-            BControlCommand::RequestSnapshot => { v.push(0x43); },
+                v.push(0x42);
+                preset.extend_midi(v);
+            }
+            BControlCommand::RequestSnapshot => {
+                v.push(0x43);
+            }
             BControlCommand::SendIdentity { id_string } => {
                 v.push(0x02);
                 extend_midi_from_string(id_string, v);
-            },
-            BControlCommand::BclReply { msg_indx, error_code } => {
-                v.push(0x21); 
+            }
+            BControlCommand::BclReply {
+                msg_indx,
+                error_code,
+            } => {
+                v.push(0x21);
                 u14_to_midi_msb_lsb(*msg_indx, v);
                 v.push(*error_code);
-            },
+            }
             BControlCommand::SendPresetName { preset, name } => {
                 v.push(0x21);
                 v.push(0);
                 preset.extend_midi(v);
                 extend_midi_from_string(name, v);
-            },
+            }
             BControlCommand::FirmwareReply { mem_addr, err } => {
                 v.push(0x35);
                 u14_to_midi_msb_lsb(*mem_addr, v);
                 v.push(*err);
-            },
-            BControlCommand::SendText => { v.push(0x78); },
+            }
+            BControlCommand::SendText => {
+                v.push(0x78);
+            }
         }
     }
 }
-
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum PresetIndex {
     Preset { index: u8 },
     All,
-    Temporary
+    Temporary,
 }
 
 impl PresetIndex {
@@ -211,7 +265,7 @@ impl PresetIndex {
             0..=31 => Ok(PresetIndex::Preset { index: m[0] }),
             0x7e => Ok(PresetIndex::All),
             0x7f => Ok(PresetIndex::Temporary),
-            n => return Err(ParseError::Invalid(format!("Bad preset index. ({n})")))
+            n => return Err(ParseError::Invalid(format!("Bad preset index. ({n})"))),
         }
     }
     fn extend_midi(self, v: &mut Vec<u8>) {
@@ -226,11 +280,9 @@ impl PresetIndex {
 fn u8_from_midi(m: &[u8]) -> Result<u8, ParseError> {
     if m.is_empty() {
         Err(ParseError::UnexpectedEnd)
-    }
-    else if m[0] > 127 {
+    } else if m[0] > 127 {
         Err(ParseError::ByteOverflow)
-    }
-    else {
+    } else {
         Ok(m[0])
     }
 }
@@ -250,10 +302,10 @@ fn u14_from_midi_msb_lsb(m: &[u8]) -> Result<u16, ParseError> {
     }
 }
 
-fn string_from_midi(m: &[u8]) -> Result<String,ParseError> {
+fn string_from_midi(m: &[u8]) -> Result<String, ParseError> {
     match String::from_utf8(m.to_vec()) {
         Ok(s) => Ok(s),
-        Err(e) => Err(ParseError::Invalid(e.to_string()))
+        Err(e) => Err(ParseError::Invalid(e.to_string())),
     }
 }
 
@@ -264,8 +316,7 @@ fn extend_midi_from_string(text: &str, v: &mut Vec<u8>) {
 fn u14_to_midi_msb_lsb(n: u16, m: &mut Vec<u8>) {
     if n > 16383 {
         panic!("Number too large to represent as two bytes of MIDI data.")
-    }
-    else {
+    } else {
         m.push(((n & 0x3f80) >> 7) as u8);
         m.push((n & 0x007f) as u8);
     }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 use clap::{Parser, Subcommand};
-use std::io::{stdin/* , stdout, Write */};
 use std::error::Error;
+use std::io::stdin;
 use std::time::Duration;
 
-use midir::{MidiIO, MidiInput, MidiOutput};
 use midi_msg::{MidiMsg, SystemExclusiveMsg};
+use midir::{MidiIO, MidiInput, MidiOutput};
 
 mod b_control;
 use b_control::*;
 
-const PGM :&str = "bcr2kosc";
+const PGM: &str = "bcr2kosc";
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     #[command(subcommand)]
-    command: Option<Commands>
+    command: Option<Commands>,
 }
 
 #[derive(Subcommand)]
@@ -25,7 +25,7 @@ enum Commands {
     /// Listen to a port and display received MIDI. Useful for debugging.
     Listen {
         /// The name of the port to listen to. Use the list command to see ports.
-        port_name: String
+        port_name: String,
     },
     /// Find and list Behringer B-Control devices.
     Find {
@@ -33,32 +33,33 @@ enum Commands {
         in_port_name: String,
         /// The name of the MIDI port to send data to.
         out_port_name: String,
-    }
+    },
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
     match &cli.command {
         Some(Commands::List {}) => Ok(list_ports()),
-        Some(Commands::Listen{ port_name }) => listen(port_name),
-        Some(Commands::Find {in_port_name, out_port_name }) 
-            => list_bcontrols(in_port_name, out_port_name),
-        None => Ok(())
+        Some(Commands::Listen { port_name }) => listen(port_name),
+        Some(Commands::Find {
+            in_port_name,
+            out_port_name,
+        }) => list_bcontrols(in_port_name, out_port_name),
+        None => Ok(()),
     }
 }
 
 fn find_port<T: MidiIO>(midi_io: &T, port_name: &str) -> Result<T::Port, Box<dyn Error>> {
     let ports = midi_io.ports();
     let wanted = Ok(port_name.to_string());
-    let port = ports.iter()
-        .find(|&x| midi_io.port_name(&x) == wanted);
+    let port = ports.iter().find(|&x| midi_io.port_name(&x) == wanted);
     match port {
         Some(p) => Ok(p.clone()),
         None => Err("MIDI port not found".into()),
     }
 }
 
-fn list_ports () {
+fn list_ports() {
     let midi_in = MidiInput::new("{PGM} list_ports").unwrap();
     print_ports("input", &midi_in);
     let midi_out = MidiOutput::new("{PGM} list_ports").unwrap();
@@ -66,7 +67,6 @@ fn list_ports () {
 }
 
 fn print_ports(dir: &str, io: &impl MidiIO) {
-
     let ports = io.ports();
     match ports.len() {
         0 => println!("No {dir} ports found"),
@@ -82,12 +82,16 @@ fn print_ports(dir: &str, io: &impl MidiIO) {
 fn listen(port_name: &str) -> Result<(), Box<dyn Error>> {
     let midi_in = MidiInput::new(&format!("{PGM} listening"))?;
     let in_port = find_port(&midi_in, port_name)?;
-    let _conn_in = midi_in.connect(&in_port, &format!("{PGM} listen connection"), 
+    let _conn_in = midi_in.connect(
+        &in_port,
+        &format!("{PGM} listen connection"),
         move |stamp, msg, _| {
             if let Ok((midi, _)) = MidiMsg::from_midi(msg) {
                 println!("{stamp}: {midi:?} (len={})", msg.len());
             }
-        }, ())?;
+        },
+        (),
+    )?;
     println!("Connection open, reading input from '{port_name}'. Press Enter to exit.");
     let mut input = String::new();
     stdin().read_line(&mut input)?;
@@ -98,41 +102,53 @@ fn listen(port_name: &str) -> Result<(), Box<dyn Error>> {
 fn list_bcontrols(in_port_name: &str, out_port_name: &str) -> Result<(), Box<dyn Error>> {
     let midi_in = MidiInput::new(&format!("{PGM} finding B-controls"))?;
     let in_port = find_port(&midi_in, in_port_name)?;
-    let _conn_in = midi_in.connect(&in_port, in_port_name, 
+    let _conn_in = midi_in.connect(
+        &in_port,
+        in_port_name,
         move |_stamp, midi_data, _context| {
             let mm = MidiMsg::from_midi(midi_data);
             if let Ok((
-                    MidiMsg::SystemExclusive {
-                        msg: SystemExclusiveMsg::Commercial {
-                            id: BEHRINGER, data
-                        }
+                MidiMsg::SystemExclusive {
+                    msg:
+                        SystemExclusiveMsg::Commercial {
+                            id: BEHRINGER,
+                            data,
+                        },
+                },
+                _, // unused size of consumed MIDI
+            )) = mm
+            {
+                // Recognized as a Behringer sysex. Parse the sysex payload.
+                let bc = BControlSysEx::from_midi(&data);
+                if let Ok((
+                    BControlSysEx {
+                        device: DeviceID::Device(dev),
+                        model: _,
+                        command: BControlCommand::SendIdentity { id_string },
                     },
-                    _ // unused size of consumed MIDI
-                )) = mm {
-                    // Recognized as a Behringer sysex. Parse the sysex payload.
-                    let bc = BControlSysEx::from_midi(&data);
-                    if let Ok((
-                            BControlSysEx{
-                                device: DeviceID::Device(dev), 
-                                model: _,
-                                command: BControlCommand::SendIdentity { id_string }
-                            },
-                            _ // unused size of consumed data
-                        )) = bc {
-                            println!("{}: {}", dev, id_string);
-                    }
+                    _, // unused size of consumed data
+                )) = bc
+                {
+                    println!("{}: {}", dev, id_string);
                 }
-            },
-        ())?;
+            }
+        },
+        (),
+    )?;
 
-    let bdata = BControlSysEx{
-        device: DeviceID::Device(0), 
-        model: BControlModel::BCR, 
-        command: BControlCommand::RequestIdentity
-    }.to_midi();
+    let bdata = BControlSysEx {
+        device: DeviceID::Device(0),
+        model: BControlModel::BCR,
+        command: BControlCommand::RequestIdentity,
+    }
+    .to_midi();
     let req = MidiMsg::SystemExclusive {
-        msg: (SystemExclusiveMsg::Commercial { id: BEHRINGER, data: bdata })
-    }.to_midi();
+        msg: (SystemExclusiveMsg::Commercial {
+            id: BEHRINGER,
+            data: bdata,
+        }),
+    }
+    .to_midi();
     let midi_out = MidiOutput::new(&format!("{PGM} finding B-controls"))?;
     let out_port = find_port(&midi_out, out_port_name)?;
     let mut conn_out = midi_out.connect(&out_port, "{PGM} finding B-controls")?;
@@ -140,4 +156,3 @@ fn list_bcontrols(in_port_name: &str, out_port_name: &str) -> Result<(), Box<dyn
     std::thread::sleep(Duration::from_millis(100));
     Ok(())
 }
-
